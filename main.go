c1dec287e9e4650620ccb03dd025fc7af7d0547b
Vladimir Satinov <vovaumni@gmail.com>
2022-03-30 20:56:01 +0300

    Main

--- main.go
+++ main.go
@@ -7,8 +7,12 @@ import (
 )
 
 func main() {
-	input := []byte("implement me")
+	input := []byte("Hello, world")
 	encoded := base64.Encode(input)
 	decoded := base64.Decode(encoded)
+	fmt.Println(encoded)
+	fmt.Println(string(encoded))
+	fmt.Println(decoded)
+	fmt.Println(string(decoded))
 	fmt.Println(bytes.Equal(input, decoded))
 }
