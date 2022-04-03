8e45803d9fc56ee73e9af7f18eeec8fc05ea3be7
Sp0ozy <imolchanov210@gmail.com>
2022-04-03 15:11:16 +0300

    done

--- base64/base64_test.go
+++ base64/base64_test.go
@@ -19,3 +19,17 @@ func TestEncode(t *testing.T) {
 		})
 	}
 }
+func TestDecode(t *testing.T) {
+	for _, tc := range []struct {
+		str  []byte
+		want []byte
+	}{
+		{[]byte{85, 86, 100, 75, 97, 108, 112, 66, 80, 84, 48, 61}, []byte{0b1010001, 0b1010111, 0b1001010, 0b1101010, 0b1011010, 0b1000001, 0b111101, 0b111101}},
+	} {
+		t.Run("", func(t *testing.T) {
+			if got := Decode(tc.str); !reflect.DeepEqual(tc.want, got) {
+				t.Errorf("Decode(%v) = %v, want %v", tc.str, got, tc.want)
+			}
+		})
+	}
+}
--- main.go
+++ main.go
@@ -7,8 +7,34 @@ import (
 )
 
 func main() {
-	input := []byte("implement me")
-	encoded := base64.Encode(input)
-	decoded := base64.Decode(encoded)
-	fmt.Println(bytes.Equal(input, decoded))
+
+	matched := true
+	for _, input := range [][]byte{
+		nil,
+		{0}, {63}, {'A'}, {255},
+		{0, 0}, {0, 255}, {'a', 'A'}, {255, 255},
+		{0, 0, 0}, {0, 0, 255}, {0, 255, 255}, {0, 'a', 'A'}, {'a', 'b', 'Z'}, {255, 255, 0}, {255, 255, 255},
+		[]byte("Many hands make light work."), // Example from https://en.wikipedia.org/wiki/Base64#Examples
+	} {
+		encoded := base64.Encode(input)
+		decoded := base64.Decode(encoded)
+
+		if bytes.Equal(input, decoded) {
+			continue
+		}
+
+		matched = false
+		fmt.Println("FAILED ", input)
+		fmt.Print("input  : ", input)
+
+		fmt.Println()
+		fmt.Print("encoded: ", encoded)
+
+		fmt.Println()
+		fmt.Printf("decoded: %d\n", decoded)
+
+	}
+	if matched {
+		fmt.Println("ALL MATCHED")
+	}
 }
