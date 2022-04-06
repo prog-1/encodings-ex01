80980f2326d20300aad32b0e291cff07c170899a
Richard Shashalevvich <richardsasalevic@gmail.com>
2022-04-06 15:54:14 +0300

    Fixed bug in base85 in  Encode

--- base85/base85.go
+++ base85/base85.go
@@ -14,30 +14,38 @@ import "fmt"
 // }
 
 func Encode(src []byte) []byte {
-	dst := make([]byte, int(len(src)/4+1)*5)
+	var dst []byte
+	if len(src)%4 == 0 {
+		dst = make([]byte, int(len(src)/4)*5)
+	} else {
+		dst = make([]byte, ((int(len(src)/4)+1)*5)+1)
+	}
 	//left := len(src) % 4
 	var b uint32
 	d := 1
 	for ; len(src) >= 4; src, d = src[4:], d+1 {
 		b = uint32(src[3]) | uint32(src[2])<<8 | uint32(src[1])<<16 | uint32(src[0])<<24
 		for i := 0; i <= 4; i++ {
-			dst[4*d-i] = '!' + byte(b%85)
+			dst[(5*d-i)-1] = '!' + byte(b%85)
 			b /= 85
 		}
 	}
 	fmt.Println(len(src))
-	switch len(src) {
-	case 3:
-		b = uint32(src[2])<<8 | uint32(src[1])<<16 | uint32(src[0])<<24
-	case 2:
-		b = uint32(src[1])<<16 | uint32(src[0])<<24
-	case 1:
-		b = uint32(src[0]) << 24
-	}
 	if len(src) != 0 {
+		switch len(src) {
+		case 3:
+			b = uint32(src[2])<<8 | uint32(src[1])<<16 | uint32(src[0])<<24
+		case 2:
+			b = uint32(src[1])<<16 | uint32(src[0])<<24
+		case 1:
+			b = uint32(src[0]) << 24
+		}
+
 		for i := 0; i <= 4; i++ {
-			dst[4*d-i] = '!' + byte(b%85)
+
+			dst[5*d-i] = '!' + byte(b%85)
 			b /= 85
+
 		}
 	}
 	return dst
--- main.go
+++ main.go
@@ -6,7 +6,8 @@ import (
 )
 
 func main() {
-	input := []byte("distinguished")
+	input := []byte("Man is distinguished, not only by his reason, but by this singular passion from other animals, which is a lust of the mind, that by a perseverance of delight in the continued and indefatigable generation of knowledge, exceeds the short vehemence of any carnal pleasure ")
+	fmt.Println()
 	encoded := base85.Encode(input)
 	//decoded := base85.Decode(encoded)
 	fmt.Println(string(encoded))
